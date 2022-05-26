119457b682345f161a190ddba9a5555fcf4aa051
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-05-27 00:20:08 +0200

    k

--- Global var.js
+++ Global var.js
@@ -1,14 +1,14 @@
 var myGlobal = 10;
 
 function fun1(){
-    oopsGlobal=5;
+    oppsGlobal=5;
 }
 function fun2(){
     var output="";
     if (typeof myGlobal !="undefined"){
         output += "myGlobal: " + myGlobal;
     }
-    if (typeof oopsGlobal !="undefined"){
+    if (typeof oppsGlobal !="undefined"){
         output += "oopsGlobal: " +oppsGlobal;
     }
     console.log(output);
